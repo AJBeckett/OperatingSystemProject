ba61768a536fd5e2f72c907bee3fad9698790e35
Jopanzo <jpnzo2k12@gmail.com>
2019-03-07 22:56:23 -0500

    Update Loader.cpp

--- Loader.cpp
+++ Loader.cpp
@@ -5,3 +5,17 @@ main{
         //insert hex or instruction into RAM
     }
 }
+#include <sstream> 
+#include "Memory.h"
+#include <queue>
+using namespace std;
+class Loader
+{
+public:
+    Loader()
+    {
+        
+    }
+    void ReadFile(string filename, Memory *memory);
+
+};
