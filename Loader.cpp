e770f8c13f45018f71d8032d9d3332223116789e
sg349 <47508175+sg349@users.noreply.github.com>
2019-03-10 01:17:53 -0500

    Add files via upload

--- Loader.cpp
+++ Loader.cpp
@@ -1,23 +1,30 @@
-main{
-     while(){
-        ReadFile();
+#include <PCB.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string line;
+    int x;
+    ifstream reader;
+    reader.open("C:\\Users\\Sam\\OSProgramFile.txt");
+    if(!reader){
+        cout << "File failed to open." << endl;
+        exit(1);
+    }
+     while(getline(reader, line)){
+        cout << line << endl;
+        if (line.length() > 0 && line.compare(3, 3, "JOB") == 0){
+            reader.close();
+        }
+        if(line == "// END"){
+            reader.close();
+        }
         //extract attributes to PCB
         //insert hex or instruction into RAM
     }
+    reader.close();
 }
-/*
-#include <sstream> 
-#include "Memory.h"
-#include <queue>
-using namespace std;
-class Loader
-{
-public:
-    Loader()
-    {
-        
-    }
-    void ReadFile(string filename, Memory *memory);
-
-};
-*/
