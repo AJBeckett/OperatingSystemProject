f8572f36904b04f1375862de41e2e2356508e127
sg349 <47508175+sg349@users.noreply.github.com>
2019-03-09 23:35:29 -0500

    Add files via upload
    Updated

--- PCB.h
+++ PCB.h
@@ -1,3 +1,25 @@
+typedef struct state{
+    
+}
+
+typedef struct registers{
+    
+}
+
+typedef struct sched{
+    float burst_time;
+    float pri;
+    float time_slice;
+    float remain_time;
+}
+
+typedef struct status{
+    bool running;
+    bool ready;
+    bool blocked;
+    bool new;
+}
+
 typedef struct PCB{
     int cpuid: // information the assigned CPU (for multiprocessor system)
     int program-counter // the job’s pc holds the address of the instruction to fetch
@@ -7,12 +29,6 @@ typedef struct PCB{
     int code-size; // extracted from the //JOB control line
     struct registers: // accumulators, index, general
     struct sched: // burst-time, priority, queue-type, time-slice, remain-time
-    struct accounts: // cpu-time, time-limit, time-delays, start/end times, io-times
-    struct memories: // page-table-base, pages, page-size
-        // base-registers – logical/physical map, limit-reg
-    struct progeny: // child-procid, child-code-pointers
-    struct parent: ptr; // pointer to parent (if this process is spawned, else ‘null’)
-    struct resources: // file-pointers, io-devices – unitclass, unit#, open-file-tables
-    int status; // {running, ready, blocked, new}
+    struct status; // {running, ready, blocked, new}
     int priority; // of the process, extracted from the //JOB control line
 }
