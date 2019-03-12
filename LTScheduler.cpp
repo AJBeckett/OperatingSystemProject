65d5da2e9449f03c630fe226495b92db52e1a32c
sg349 <47508175+sg349@users.noreply.github.com>
2019-03-12 00:06:28 -0400

    Add files via upload

--- LTScheduler.cpp
+++ LTScheduler.cpp
@@ -1,3 +1,22 @@
-main{
-    
+#include <Memory.h>
+#include <queue>
+
+using namespace std;
+
+void LTScheduler(){
+    PCB p;
+    int pri = 100;
+    for(int i = 0; i < 30; i++){
+        for(int j = i; j < 30; j++){
+            if(PCB_arr[j].priority < pri){
+                p = PCB_arr[j];
+                PCB_arr[j] = PCB_arr[i];
+                PCB_arr[i] = p;
+                pri = p.priority;
+            }
+        }
+    }
+    for(int i = 0; i < 30; i++){
+        rq.push(PCB_arr[i]);
+    }
 }
