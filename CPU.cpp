63d0acb5cff14f2529df0c54792dfc9d3645f425
Jopanzo <jpnzo2k12@gmail.com>
2019-03-13 15:33:19 -0400

    Update CPU.cpp

--- CPU.cpp
+++ CPU.cpp
@@ -1,44 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <PCB.h>
-fetch(){
+#include <PCB.h> //Johnny: Figured we need pcb.h for cpu
+class CPU
+{
+ fetch(){
     
-}
+ }
 
-decode(){
+ decode(){
     
-}
+ }
 
-execute(){
+ execute(){
     
-}
+ }
 
-DMA(){
-    while(){
-        switch(type){
+ DMA(){
+    while()
+    {
+        switch(type)
+        {
             case 0: Read(ch, next(p_rec), buf[next_io]);
             case 1: Write(ch, next(p_rec), buf[next_io]);
         }
         next_io := next_io + 4;
     }
     signal(ComputeOnly);
-}
+ }
 
-ComputeOnly(){
-    while(){
+ComputeOnly()
+{
+    while()
+    {
         ir : = Fetch(memory[map(PC)]);// fetch instruction at RAM address – mapped PC
         OSDriver::decode(ir, oc, addrptr); // part of decoding of the instruction in instr reg (ir),
         //returning the opcode (oc) and a pointer to a list of
         //significant addresses in ‘ir’ – saved elsewhere
         PC := PC + 1; // ready for next instruction, increase PC by 1 (word)
-        execute(oc) {
+        execute(oc) 
+        {
         case 0: // corresponding code using addrptr of operands
         case 1: // corresponding code or send interrupt
         …
         }
     }
-}
-
+ }
+};
+#endif
+/*
 main(){
     
-}
+} */
+
